b9ec132aa0570fb83ee0383142e5a5bede21881b
lbuttignol <leandrobuttignol@gmail.com>
2020-05-10 00:22:54 -0300

    database allow unicode characters

--- api/src/sql/database.js
+++ api/src/sql/database.js
@@ -1,16 +1,51 @@
-const mysql = require('mysql2');
+const mysql = require( 'mysql' );
 
+// const config = {
+//   // connectionLimit: 100,
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE
+// }
+
+// Mysql config params
 const config = {
-  connectionLimit: 100,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  host: 'localhost',
+  user: 'mysql',
+  password: 'example',
+  database: 'footballdb'
 }
 
-// create the connection
-// const con = mysql.createConnection(config);
-const con = mysql.createPool(config);
+class Database {
+    // constructor( conf ) {
+    //     this.connection = mysql.createConnection( conf );
+    // }
+    constructor() {
+        this.connection = mysql.createConnection( config );
+    }
+    
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
 
-module.exports = con;
+module.exports = Database;
 
--- api/src/sql/database.sql
+++ api/src/sql/database.sql
@@ -1,38 +1,48 @@
 -- DROP DATABASE IF EXISTS footballDb;
-CREATE DATABASE IF NOT EXISTS footballdb;
+CREATE DATABASE IF NOT EXISTS footballdb 
+  CHARACTER SET utf8mb4 
+  COLLATE utf8mb4_unicode_ci;
+-- SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE footballdb
+USE footballdb;
 
 CREATE TABLE IF NOT EXISTS competitions (
   id        INT NOT NULL PRIMARY KEY,
   name      VARCHAR(255),
   code      VARCHAR(3) NOT NULL,
   areaName  VARCHAR(255)
-);
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS teams (
   id        INT NOT NULL PRIMARY KEY,
-  name      VARCHAR(255),
-  tla       VARCHAR(255),
-  shortName VARCHAR(255),
-  areaName  VARCHAR(255),
-  email     VARCHAR(255)
-);
+  name      VARCHAR(255) ,
+  tla       VARCHAR(255) ,
+  shortName VARCHAR(255) ,
+  areaName  VARCHAR(255) ,
+  email     VARCHAR(255) 
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS comp_teams (
+  competition_id  INT NOT NULL ,
+  teams_id        INT NOT NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 
 CREATE TABLE IF NOT EXISTS players (
   id              INT NOT NULL PRIMARY KEY,
-  name            VARCHAR(255),
-  position        VARCHAR(255),
+  name            VARCHAR(255) ,
+  position        VARCHAR(255) ,
   dateOfBirth     DATE,
-  countryOfBirth  VARCHAR(255),
-  nacionality     VARCHAR(255)
-);
+  countryOfBirth  VARCHAR(255) ,
+  nacionality     VARCHAR(255) 
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 
 INSERT INTO competitions (id, name,code, areaName) values (
 1, 'compe1','cmp','argentina');
 
 INSERT INTO teams (id, name,tla, shortName, areaName,email) values (
-1, 'equipo1','eq1','argentina','bla','bla');
+1, 'Qarabağ Ağdam FK','eq1','argentina','bla','bla');
 
 INSERT INTO players (id, name,position, dateOfBirth, countryOfBirth,nacionality) values (
 1, 'equipo1','eq1',NULL,NULL,NULL);
